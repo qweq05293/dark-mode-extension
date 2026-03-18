ccc9e908d1e7d19c333442edca093b3c8b8f0e90
qweq05293 <qweq05293@gmail.com>
2026-03-18 17:56:25 +0000

    add content to build

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, "index.html"),
         background: path.resolve(__dirname, "src/background.ts"),
+        content: path.resolve(__dirname, "src/content.ts"),
       },
       output: {
         entryFileNames: "[name].js",
